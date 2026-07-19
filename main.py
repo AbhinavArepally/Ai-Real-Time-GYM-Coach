8c3e30e5710a5e40560bec1bb3040942fb7d2002
Abhinav Arepally <arepallyabhinav7@gmail.com>
2026-07-19 22:12:15 +0530

    remove opencv conflict

--- main.py
+++ main.py
@@ -201,7 +201,29 @@ def main():
             key="exercise-analysis",
             mode=WebRtcMode.SENDRECV,
             video_processor_factory=VideoProcessorClass,
-            rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+            #rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+            rtc_configuration={
+                "iceServers": [
+                    {
+                        "urls": "stun:global.stun.twilio.com:3478"
+                    },
+                    {
+                        "username": "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+                        "credential": "tE2DajzSJwnsSbc123",
+                        "urls": "turn:global.turn.twilio.com:3478?transport=udp"
+                    },
+                    {
+                        "username": "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+                        "credential": "tE2DajzSJwnsSbc123",
+                        "urls": "turn:global.turn.twilio.com:3478?transport=tcp"
+                    },
+                    {
+                        "username": "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+                        "credential": "tE2DajzSJwnsSbc123",
+                        "urls": "turn:global.turn.twilio.com:443?transport=tcp"
+                    }
+                ]
+            },
             media_stream_constraints={
                 "video": True,
                 "audio": False
