95d5c425283b2ff8a0edbc95d1ce196963e63502
Abhinav Arepally <arepallyabhinav7@gmail.com>
2026-07-19 20:36:28 +0530

    remove opencv conflict

--- main.py
+++ main.py
@@ -201,7 +201,17 @@ def main():
             key="exercise-analysis",
             mode=WebRtcMode.SENDRECV,
             video_processor_factory=VideoProcessorClass,
-            rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+            #rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+            rtc_configuration={
+                    "iceServers": [
+                        {
+                            "urls": [
+                                "stun:stun.l.google.com:19302",
+                                "stun:stun1.l.google.com:19302"
+                            ]
+                        }
+                    ]
+                },
             media_stream_constraints={
                 "video": True,
                 "audio": False
